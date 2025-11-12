b4dddb8b8108d23d7660c1acf22e3fbbc75fc747
Sergey Kozyr <thelichking9@gmail.com>
2025-11-12 21:08:50 +0300

    add verbose names

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -11,14 +11,23 @@ class PokemonEntityManager(Manager):
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, null=True, blank=True)
-    title_jp = models.CharField(max_length=200, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
+    title = models.CharField("Название на русском", max_length=200)
+    title_en = models.CharField("Название на английском", max_length=200, null=True, blank=True)
+    title_jp = models.CharField("Название на японском", max_length=200, null=True, blank=True)
+    image = models.ImageField("Изображение", null=True, blank=True)
+    description = models.TextField("Описание", null=True, blank=True)
     previous_evolution = models.ForeignKey(
-        "self", on_delete=models.SET_NULL, null=True, blank=True, related_name="next_evolutions"
+        "self",
+        verbose_name="Из кого эволюционировал",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="next_evolutions",
     )
+
+    class Meta:
+        verbose_name = "Покемон"
+        verbose_name_plural = "Покемоны"
 
     def __str__(self) -> str:
         return self.title
@@ -30,13 +39,20 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     objects = PokemonEntityManager()
 
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="entities")
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeares_at = models.DateTimeField()
-    disappeares_at = models.DateTimeField()
-    level = models.IntegerField(default=1)
-    health = models.IntegerField(default=1)
-    damage = models.IntegerField(default=1)
-    defence = models.IntegerField(default=1)
-    stamina = models.IntegerField(default=1)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="entities", verbose_name="Покемон")
+    lat = models.FloatField("Широта")
+    lon = models.FloatField("Долгота")
+    appeares_at = models.DateTimeField("Появляется в")
+    disappeares_at = models.DateTimeField("Исчезает в")
+    level = models.IntegerField("Уровень", default=1)
+    health = models.IntegerField("Здоровье", default=1)
+    damage = models.IntegerField("Атака", default=1)
+    defence = models.IntegerField("Защита", default=1)
+    stamina = models.IntegerField("Выносливость", default=1)
+
+    class Meta:
+        verbose_name = "Сущность покемона"
+        verbose_name_plural = "Сущности покемонов"
+
+    def __str__(self) -> str:
+        return f"{self.pokemon.title} {self.level} уровня"
